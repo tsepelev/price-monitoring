debdbce691db9a06848357024652c69a105f8b6f
Sergey Tsepelev <tsepelev@yandex.ru>
2025-02-15 22:25:39 +0300

    Добавить функциональность поиска товаров через API и обновить шаблон

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
+import os
+import requests
 from flask import Flask, render_template
+from flask import request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return render_template('index.html', content='Hello, World!')
 
-if __name__ == '__main__':
+@app.route("/")
+def index():
+    query = request.args.get("q")
+    url = "https://www.searchapi.io/api/v1/search"
+    params = {
+        "engine": "google_shopping",
+        "q": query,
+        "gl": "ru",
+        "hl": "ru",
+        "location": "Moscow,Russian Federation",
+        "api_key": os.environ.get("SEARCH_API_KEY"),
+    }
+    resp = ""
+    if query:
+        # response = requests.get(
+        #     url,
+        #     params=params,
+        # )
+        response = requests.get(
+            "https://www.searchapi.io/api/v1/searches/search_6d97LXP4moruanBXWk03EOrl"
+        )
+        resp = response.json()
+    return render_template("index.html", content=resp)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
