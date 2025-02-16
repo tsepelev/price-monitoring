7add63209cd0e8496585cffadb3b3733c8af5916
Sergey Tsepelev <tsepelev@yandex.ru>
2025-02-16 09:16:22 +0300

    Добавить поддержку переменной окружения DEBUG и обновить логику запуска приложения

--- app.py
+++ app.py
@@ -17,21 +17,22 @@ def index():
         "q": query,
         "gl": "ru",
         "hl": "ru",
-        "location": "Moscow,Russian Federation",
+        "location": "Moscow",
         "api_key": os.environ.get("SEARCH_API_KEY"),
     }
     resp = ""
     if query:
-        # response = requests.get(
-        #     url,
-        #     params=params,
-        # )
         response = requests.get(
-            "https://www.searchapi.io/api/v1/searches/search_6d97LXP4moruanBXWk03EOrl"
+            url,
+            params=params,
         )
-        resp = response.json()
+        # response = requests.get(
+        #     url="https://www.searchapi.io/api/v1/searches/search_6d97LXP4moruanBXWk03EOrl"
+        # )
+        if response.status_code == 200:
+            resp = response.json()
     return render_template("index.html", content=resp)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=os.environ.get("DEBUG", False))
